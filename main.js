74ecc3dc6c1edd6fee6575d358a6974361804bd2
abhiagrawal9 <abhi2703agrawal@gmail.com>
2021-05-15 19:36:53 +0530

    Electron app

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const { app, BrowserWindow } = require('electron');
-const url = require('url');
 const path = require('path');
 
-let mainWindow;
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-app.on('ready', () => {
-  // Create New Window
-  mainWindow = new BrowserWindow({});
+  win.loadFile('index.html');
+}
 
-  // Load html into window
-  mainWindow.loadFile('index.html');
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
